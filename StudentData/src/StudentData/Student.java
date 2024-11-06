2442e68b267c33806368457622975990ae450b00
unknown <Balakrishna4428@gmail.com>
2024-11-06 07:37:59 +0530

    US: 123 : create login and logout functionality

---
+++ StudentData/src/StudentData/Student.java
@@ -0,0 +1,21 @@
+package StudentData;
+
+public class Student {
+
+	int sid; String iname; char igen;
+	
+	void student() {
+		System.out.println("Please wait your data is loading "+sid+" "+iname+" "+igen);
+	}
+	
+	Student(int id, String name, char gen){
+		sid=id;
+		iname=name;
+		igen=gen;
+	}
+	Student(){
+		sid=202;
+		iname="Krishna";
+		igen='M';
+	}
+}
---
+++ StudentData/src/StudentData/StudentMain.java
@@ -0,0 +1,11 @@
+package StudentData;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		//Student obj = new Student(101, "Balu", 'M');
+		Student obj = new Student();
+		obj.student();
+		
+	}
+}
